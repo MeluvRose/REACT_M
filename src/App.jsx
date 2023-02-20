f33add21ddd8328aa27dcdf82e9249fdaff5e8b9
meluvrose <tanggu3910@gmail.com>
2023-02-20 16:16:25 +0900

    #2.2 Adapting and Extending

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: magenta;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: skyblue;
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.span`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
-  /*
-  return (
-    <div style={{ display: "flex" }}>
-      <div
-        style={{ backgroundColor: "magenta", width: 100, height: 100 }}
-      ></div>
-      <div
-        style={{ backgroundColor: "skyblue", width: 100, height: 100 }}
-      ></div>
-    </div>
-  );
-  */
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
