85157c0760dd2167119c173391408bb11c0d392b
meluvrose <tanggu3910@gmail.com>
2023-02-20 17:47:07 +0900

    #2.7 Themes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import styled, { keyframes } from "styled-components";
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 const animation = keyframes`
@@ -21,6 +26,10 @@ const animation = keyframes`
 
 const Emoji = styled.span`
   font-size: 48px;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 const Box = styled.div`
@@ -44,6 +53,7 @@ const Box = styled.div`
 function App() {
   return (
     <Container>
+      <Title>Title</Title>
       <Box>
         <Emoji as="p">😁</Emoji>
       </Box>
