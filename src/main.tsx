098845e1c121e03eb5fc8f5e63d929fffe4ca11e
meluvrose <tanggu3910@gmail.com>
2023-02-21 17:17:06 +0900

    #3.6 Themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
 // ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
