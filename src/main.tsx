437f3eb4287d367fbbd651f56a0cd4afdca882f2
meluvrose <tanggu3910@gmail.com>
2023-02-22 19:40:20 +0900

    #5.0 Setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme, lightTheme } from "./theme";
+import { theme } from "./theme";
 
 // ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
