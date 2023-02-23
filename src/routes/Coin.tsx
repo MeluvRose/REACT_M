7935b8d55408a85864f1d0da964f2fcc4e45c2da
meluvrose <tanggu3910@gmail.com>
2023-02-23 15:28:06 +0900

    5.12 Price Chart part 1

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -164,24 +164,8 @@ function Coin() {
     ["tickers", coinId],
     () => fetchCoinTicker(coinId)
   );
-  // const [loading, setLoading] = useState(true);
-  // const [info, setInfo] = useState<InfoData>();
-  // const [priceInfo, setPriceInfo] = useState<PriceData>();
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const infoData = await (
-  //       await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-  //     ).json();
-  //     const priceData = await (
-  //       await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-  //     ).json();
-  //     setInfo(infoData);
-  //     setPriceInfo(priceData);
-  //     setLoading(false);
-  //   })();
-  // }, [coinId]);
   const loading = infoLoading || tickersLoading;
+
   return (
     <Container>
       <Header>
@@ -232,7 +216,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/nomadcoin/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
